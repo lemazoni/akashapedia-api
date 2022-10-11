6f6729c5d5341194ba3d47d9d0dbc8660a4e2709
lemazoni <mazoni.le@gmail.com>
2022-10-11 17:32:34 -0300

    first commit of api to heroku

---
+++ ormconfig.js
@@ -0,0 +1,22 @@
+console.log(process.env.DB_HOST);
+module.exports = {
+  "type": "mysql",
+  "host": process.env.HOST,
+  "port": process.env.PORT,
+  "synchronize": false,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASS,
+  "database": process.env.DATABASE,
+  "entities": [
+    "dist/models/**/*.js"
+ ],
+ "migrations": [
+  "dist/migrations/**/*.js"
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations/"
+  ],
+  "entitiesDir": "src/models"
+  }
+}
