eeab3d0adc7f86c38ecfd0072fb648340d221dcc
lemazoni <mazoni.le@gmail.com>
2022-10-11 19:28:45 -0300

    akashapedia api started

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
+  "type": "mysql",
+  "host": "us-cdbr-east-06.cleardb.net",
+  "port": 3306,
+  "username": "b7a13257094872",
+  "password": "b27bad24",
+  "database": "heroku_d4bb46ee197b208",
   "entities": [
     "dist/models/**/*.js"
   ],
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
 import app from './app';
 import 'reflect-metadata';
 import './database';
-
-dotenv.config();
 
 app.listen(3000, () => {
   console.log('🏃 Running Server');
